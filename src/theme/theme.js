edc7d9258ccd8bc0b3ce00cb9c71d9f087d92239
Nivas <46569236+Nivashawk@users.noreply.github.com>
2025-04-10 18:00:15 +0530

    basic theme setup done

---
+++ src/theme/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    background: '#FFFFFF',
+    primary: '#004643',
+    secondary: '#F4F4F4',
+    text_color_1: '#000000',
+    text_color_2: 'C4C4C4',
+    status_green: "#80D97E",
+    status_red: "#DA4035"
+  };
